e0d52dc5e659b8d6027d0be0d969c53062b97978
ВАШЕ Имя <marina013@bk.ru>
2023-01-16 11:21:25 +0300

    Решение 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+string str1 = Console.ReadLine();
+int num1 = Convert.ToInt32(str1);
+
+Console.WriteLine("Введите второе число");
+string str2 = Console.ReadLine();
+int num2 = Convert.ToInt32(str2);
+
+if (num1>num2)
+{
+    Console.WriteLine($"Первое число {num1} больше второго числа {num2}");
+}
+else if (num1==num2)
+{
+    Console.WriteLine("Числа равны");
+}
+else Console.WriteLine($"Второе число {num2} больше первого числа {num1}");
