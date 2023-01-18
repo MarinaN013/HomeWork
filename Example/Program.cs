bb50918611c289f0f2cbbb696ce46648b659b2aa
ВАШЕ Имя <marina013@bk.ru>
2023-01-18 13:20:25 +0300

    Решение

---
+++ Example/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+
+if (number>999)
+ {
+    while (number>999)
+    {
+        number = number/10;
+    }
+ }
+if (number>100&number<1000)
+{
+    result = number%10;
+    Console.WriteLine(result);
+}
+else
+{
+   Console.WriteLine("Число не трехзначное"); 
+}
+
+
+
+
--- HW_2/Task_22/Program.cs
+++ HW_2/Task_22/Program.cs
@@ -22,3 +22,28 @@ else
 {
     Console.WriteLine("Число является не трехзначным");
 }
+
+
+/*Решение без перевода числа в строку
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+
+if (number>999)
+ {
+    while (number>999)
+    {
+        number = number/10;
+    }
+ }
+if (number>100&number<1000)
+{
+    result = number%10;
+    Console.WriteLine(result);
+}
+else
+{
+   Console.WriteLine("Число не трехзначное"); 
+}
+*/
