6cd36b1e54cec94119958d708d8b1aa0cd880b2f
ВАШЕ Имя <marina013@bk.ru>
2023-02-28 09:58:11 +0300

    Решение

---
+++ Final_Project/Program.cs
@@ -0,0 +1,30 @@
+﻿
+string text = "ому,в,та, произошло";
+string[] words = text.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+int count=0;
+  foreach (string s in words)
+  {
+    if (s.Length<=3)
+    {
+        count++;
+    }
+  }
+
+string[] newArr = new string[count];
+int i=0;
+foreach (string s in words)
+  {
+    if (s.Length<=3)
+    {
+        newArr[i] = s;
+        i++;
+    }
+  }
+
+for (int c = 0; c < newArr.Length; c++)
+{
+    Console.Write($"{newArr[c]} ");
+}
+Console.WriteLine(newArr.Length);
+
