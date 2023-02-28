58ce1abcdde05af6f5675aa0e09f6503dc2db3db
ВАШЕ Имя <marina013@bk.ru>
2023-02-28 10:53:01 +0300

    Решение

--- Final_Project/Program.cs
+++ Final_Project/Program.cs
@@ -1,17 +1,23 @@
 ﻿
-string text = "ому,в,та, произошло";
+Console.WriteLine("Введите строку символов, разделенных запятыми");
+string text = Console.ReadLine();
 string[] words = text.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
 
-int count=0;
+int FindSumString (string [] words) //Находим сумму элементов, кторые меньше или равны 3
+{
+int sum=0;
   foreach (string s in words)
   {
     if (s.Length<=3)
     {
-        count++;
+        sum++;
     }
   }
+  return sum;//
+}
 
-string[] newArr = new string[count];
+void CreateNewArr(string [] words, string [] newArr)//Записываем в новый массив, все строки которые меньше или равны 3
+{
 int i=0;
 foreach (string s in words)
   {
@@ -21,10 +27,17 @@ foreach (string s in words)
         i++;
     }
   }
-
-for (int c = 0; c < newArr.Length; c++)
-{
-    Console.Write($"{newArr[c]} ");
 }
-Console.WriteLine(newArr.Length);
 
+void PrintNewArr(string [] newArr )//Выводим на консоль все строки нового массива
+{
+for (int j = 0; j < newArr.Length; j++)
+{
+    Console.Write($"{newArr[j]} ");
+}
+}
+
+int count=FindSumString(words);
+string[] newArr = new string[count];
+CreateNewArr(words,newArr);
+PrintNewArr(newArr);
