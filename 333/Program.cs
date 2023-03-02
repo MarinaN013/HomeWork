19ac78f1c454e049307d0b09c4a0b9d62fb367c3
ВАШЕ Имя <marina013@bk.ru>
2023-03-02 20:29:47 +0300

    Решение

---
+++ 333/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] Action(string[] text) 
+{
+    int number = 0;
+    for(int index = 0; index < text.Length; index++)
+    {
+        if(text[index].Length <= 3)
+            number++;        
+    }    
+    string[] result = new string [number];
+    for(int index1 = 0, index2 = 0; index1 < text.Length; index1++)
+    {
+        if(text[index1].Length <= 3)
+            result[index2++] = text[index1];
+    }    
+    return result;
+}
+
+
+string[] Data = {"Программа", "нос", "строка", "007", "21", "лучше", "-234"};
+
+Console.Write("[{0}] -> [{1}] ", string.Join(", ", Data), string.Join(", ", Action(Data)));
